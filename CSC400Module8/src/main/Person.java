c1f465a3cb63c4e32ba4f99ada90d124ef211947
Maynard Peralta <maynperalta@gmail.com>
2026-03-07 10:04:45 -0700

    Person class added

--- CSC400Module8/src/main/Person.java
+++ CSC400Module8/src/main/Person.java
@@ -2,11 +2,16 @@ package main;
 
 public class Person {
 	
-	String firstname;
-	String lastname;
+	String firstName;
+	String lastName;
 	int age;
 	
-	public Person(String firstName, String lastName, int age);
-	
-
+	public Person(String firstName, String lastName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	public String toString() {
+		return lastName + ", " + firstName + " (Age: " + age + ")";
+	}
 }
