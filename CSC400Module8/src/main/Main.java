81fdf268c74eb9196fbca006d1f0940294fa8a87
Maynard Peralta <maynperalta@gmail.com>
2026-03-07 11:16:47 -0700

    Added input validation to Main.java

--- CSC400Module8/src/main/Main.java
+++ CSC400Module8/src/main/Main.java
@@ -7,21 +7,50 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		Queue queue = new Queue();
+		final int NUM_PEOPLE = 5;
 		
 		System.out.println("Enter name and age information for 5 people: ");
 		
-		for(int i = 0; i < 5; ++i) {
+		for(int i = 0; i < NUM_PEOPLE; ++i) {
 			System.out.println("\nPerson " + (i + 1));
 			
-			System.out.print("First Name: ");
-			String first = scnr.nextLine();
+			String first;
+			while (true) {
+				System.out.print("First Name: ");
+				first = scnr.nextLine().trim();
+				if(!first.isEmpty()) {
+					break;
+				}
+				System.out.println("First name cannot be blank.");
+			}
 			
-			System.out.print("Last Name: ");
-			String last = scnr.nextLine();
+			String last;
+			while (true ) {
+				System.out.print("Last Name: ");
+				last = scnr.nextLine().trim();
+				if(!last.isEmpty()) {
+					break;
+				}
+				System.out.println("Last name cannot be blank.");
+			}
 			
-			System.out.print("Age: ");
-			int age = scnr.nextInt();
-			scnr.nextLine();
+			int age = -1;
+			while(true) {
+				System.out.print("Age: ");
+			
+				if (scnr.hasNextInt()) {
+					age = scnr.nextInt();
+					scnr.nextLine();
+					if (age > 0 && age <= 120) {
+						break;
+					} else {
+						System.out.println("Age must be between 1 and 120.");
+					}
+				} else {
+					System.out.println("Invalid input. Please enter an integer.");
+					scnr.nextLine();
+				}
+			}
 			
 			queue.enqueue(new Person(first, last, age));
 		}
