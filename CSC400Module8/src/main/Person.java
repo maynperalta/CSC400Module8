81e2534d7ec141ef51ed8349ce2f7613f171b28a
Maynard Peralta <maynperalta@gmail.com>
2026-03-08 11:36:19 -0600

    Added setter and getter methods to Person.java

--- CSC400Module8/src/main/Person.java
+++ CSC400Module8/src/main/Person.java
@@ -1,16 +1,36 @@
 package main;
 // Person class with first name, last name, and age
 public class Person {
-	String firstName;
-	String lastName;
-	int age;
+	private String firstName;
+	private String lastName;
+	private int age;
 // Constructor to create a person object	
 	public Person(String firstName, String lastName, int age) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.age = age;
 	}
+// Setter and getter methods	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public int getAge() {
+		return age;
+	}
 // Return a string of person	
+	@Override
 	public String toString() {
 		return lastName + ", " + firstName + " (Age: " + age + ")";
 	}
